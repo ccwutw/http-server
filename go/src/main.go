ab5c48491ad04ccadbed1fffff2004f79c07dfcb
Chien-Cheng Wu <55981989+ccwutw@users.noreply.github.com>
2024-09-27 07:48:42 +0800

    add python implementation

--- go/src/main.go
+++ go/src/main.go
@@ -119,4 +119,89 @@ func main() {
 	router.HandleFunc("/", handleGet).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8080", router))
 
+	/*
+		os.Remove("./foo.db")
+
+		db, err := sql.Open("sqlite3", "./foo.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+
+		sqlStmt := `
+		create table foo (id integer not null primary key, name text);
+		delete from foo;
+		`
+		_, err = db.Exec(sqlStmt)
+		if err != nil {
+			log.Printf("%q: %s\n", err, sqlStmt)
+			return
+		}
+
+		tx, err := db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer stmt.Close()
+		for i := 0; i < 100; i++ {
+			_, err = stmt.Exec(i, fmt.Sprintf("Hello %03d", i))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		err = tx.Commit()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rows, err := db.Query("select id, name from foo")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var id int
+			var name string
+			err = rows.Scan(&id, &name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(id, name)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+
+		_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rows, err = db.Query("select id, name from foo")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		for rows.Next() {
+			var id int
+			var name string
+			err = rows.Scan(&id, &name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(id, name)
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
+
 }
